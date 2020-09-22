536a70d72bd859f6be5b80f378a6f9e7c1df6df2
Dimi <64038279+DimiPavlov@users.noreply.github.com>
2020-09-22 14:20:40 +0300

    Updates

---
+++ precache-manifest.41b9e2962613ced7e804e07c6c8bb828.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5b03a06ea53e41bf80f95f94bc33d61",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "97c5e27e38d2456e1ee5",
+    "url": "/monsters/static/css/main.c02662a0.chunk.css"
+  },
+  {
+    "revision": "78adac64eaa07ff35dea",
+    "url": "/monsters/static/js/2.356d906f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters/static/js/2.356d906f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97c5e27e38d2456e1ee5",
+    "url": "/monsters/static/js/main.d959346a.chunk.js"
+  },
+  {
+    "revision": "3dab8aa598718cb8e8b3",
+    "url": "/monsters/static/js/runtime-main.f48c3991.js"
+  }
+]);
